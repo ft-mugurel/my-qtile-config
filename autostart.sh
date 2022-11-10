39b2d702960f9e0960f62e0d5bd50e413f7b9e3e
Muhammed Talha Ugurel <muhammedtalhaugurel@gmail.com>
2022-11-11 01:11:16 +0300

    V1.0

---
+++ autostart.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+function run {
+  if ! pgrep $1 ;
+  then
+    $@&
+  fi
+}
+
+userresources=$HOME/.Xresources
+usermodmap=$HOME/.Xmodmap
+sysresources=/etc/X11/xinit/.Xresources
+sysmodmap=/etc/X11/xinit/.Xmodmap
+
+if [ -f $sysresources ]; then
+    xrdb -merge $sysresources
+fi
+
+if [ -f "$userresources" ]; then
+    xrdb -merge "$userresources"
+fi
+
+# start some nice programs
+
+if [ -d /etc/X11/xinit/xinitrc.d ] ; then
+ for f in /etc/X11/xinit/xinitrc.d/?*.sh ; do
+  [ -x "$f" ] && . "$f"
+ done
+ unset f
+fi
+
+if [ -f $usermodmap ]; then
+    xmodmap $usermodmap
+else
+  xmodmap $sysmodmap
+fi
+
+if [ -e /sys/class/power_supply/BAT0 ]; then
+    export IS_LAPTOP=$true
+fi
+
+variety &
+/usr/bin/emacs --daemon &
+lxsession &
+pulsemixer --unmute &
+picom -b &
+parcellite &
+unclutter &
+nm-applet &
+udiskie --smart-tray &
+redshift-gtk &
+
+run dex $HOME/.config/autostart/arcolinux-welcome-app.desktop
+run nm-applet
+#run caffeine
+run pamac-tray
+#run variety
+run xfce4-power-manager
+run blueberry-tray
+run /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
+run numlockx on
+run volumeicon
+#run nitrogen --restore
+run conky -c $HOME/.config/awesome/system-overview
+
+#feh --bg-fill /usr/share/backgrounds/arcolinux/arco-wallpaper.jpg &
